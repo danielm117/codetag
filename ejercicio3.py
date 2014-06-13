5c0cf8021c6fcc98fc8fd3013bfbb9ce355049ba
root <root@daniel-HP-Pavilion-dv4-Notebook-PC.(none)>
2014-06-12 23:44:01 -0500

    15 archivos

---
+++ ejercicio3.py
@@ -0,0 +1,24 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+#suma y divide e imprime el resultado
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+#imprime el boolean de la comparacion
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more"
+
+#imprime el boolean de la comparacion mas el texto
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
